b72d6ec1dbb09e73fb21e2885d3d61d7ed9f7a72
AlanRetamar <76825944+AlanRetamar@users.noreply.github.com>
2020-12-31 19:14:17 -0300

    Subo por primera vez la pagina

---
+++ ranking/assets/script/script.js
@@ -0,0 +1,18 @@
+
+let links = document.querySelectorAll(".close")
+
+links.forEach(function(link) {
+    link.addEventListener("click", function(ev) {
+      ev.preventDefault()
+      let content = document.querySelector(".content")
+
+      content.classList.remove("animate__animated")
+      content.classList.remove("animate__bounceInDown")
+      content.classList.add("animate__animated")
+      content.classList.add("animate__bounceOutUp")
+
+      setTimeout(function () {
+        location.href = "/"
+      }, 600)
+    })
+}) 
