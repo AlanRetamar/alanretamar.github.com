b1b8adb7c4c3900cd8a83f5e29e246610363c8d5
AlanRetamar <76825944+AlanRetamar@users.noreply.github.com>
2021-04-09 23:13:10 -0300

    Subo mi primera pagina web

---
+++ technews/js/app.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-bar').addEventListener('click', function (){
+  document.querySelector('.nav-menu').classList.toggle('show')
+})
+
+// ScrollReveal().reveal('body',{ delay:500 });
+ScrollReveal().reveal('.nav-main',{ delay:500 });
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards',{ delay:500 });
+ScrollReveal().reveal('.card-banner',{ delay:500 });
+ScrollReveal().reveal('.card-banner-two',{ delay:500 });
+
+setTimeout(function () {
+  location.href = "/technews"
+}, 600)
